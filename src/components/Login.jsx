1e0cf032a9ab6836067ff928f5c349c55c7d3855
Ricardo Arturo <49619270+Ricaidito@users.noreply.github.com>
2022-07-12 00:32:26 -0400

    Fixed naming

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,26 @@
-import '../Styles/Login.css';
+import "../Styles/Login.css";
 
 const Login = () => {
-    return(
-        <div className='Login_container'>
-            <h1> Sign In </h1>
-            <form>
-                <label for="username">Email adress</label>
-                <input type="text" placeholder="Enter email" required></input>
-                <label for="password">Password</label>
-                <input type="password" placeholder="Enter password" required></input>
-                <label>
-                    <input type="checkbox" className='Login_remember'></input> Remember me
-                </label>
-                <button type='Submit' className='Login_submit'>Submit</button>
-                <span class="Login_psw">Forgot <a href="" className='Login_link'>password?</a></span>
-            </form>
-        </div>
-    );
+  return (
+    <div className="Login_container">
+      <h1>Sign In</h1>
+      <form>
+        <label for="username">Email address</label>
+        <input type="text" placeholder="Enter email" required></input>
+        <label for="password">Password</label>
+        <input type="password" placeholder="Enter password" required></input>
+        <label>
+          <input type="checkbox" className="Login_remember"></input> Remember me
+        </label>
+        <button type="Submit" className="Login_submit">
+          Submit
+        </button>
+        <span class="Login_psw">
+          Forgot <a href="placeholder">password?</a>
+        </span>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
